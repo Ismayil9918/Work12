b653400256d02d439ba568e776d06c59549cb8a6
Ismayil9918 <ismayilbi@code.edu.az>
2022-02-22 07:35:24 +0400

    Ev tapsirigi

---
+++ job2022/Group.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace job2022
+{
+    internal class Group
+    {
+        public string No;
+        public int Limit;
+
+        private Student[] _students;
+
+        public Group()
+        {
+            _students=new Student[0];
+        }
+        public void AddStudent(Student student)
+        {
+            if (_students.Length<Limit)
+            {
+
+            }
+        }
+    }
+}
---
+++ job2022/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace job2022
+{
+    internal class Student
+    {
+        public string Name;
+        public string Surname;
+    }
+}
